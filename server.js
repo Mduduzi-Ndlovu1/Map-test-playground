d502c5e478a41c5c901b36ed372f20802f09342f
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-16 11:01:50 +0200

    sewrver cors change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://1pulse-playground.netlify.app/', 
+  origin: 'https://1pulse-playground.netlify.app', 
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 })); 
